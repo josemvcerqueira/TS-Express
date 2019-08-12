ff9283fbda13c09a8c53581781bd5ce225f4a5a8
Jose Cerqueira <jose@origensmedia.com>
2019-08-12 01:21:05 +0100

    Added a custom login router and basic html form

--- build/index.js
+++ build/index.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("hello");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var body_parser_1 = __importDefault(require("body-parser"));
+var login_routes_1 = __importDefault(require("./routes/login.routes"));
+var app = express_1.default();
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(login_routes_1.default);
+app.listen(3000, function () {
+    console.log("Listening on port 3000");
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello")
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+
+import loginRouter from "./routes/login.routes";
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(loginRouter);
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
